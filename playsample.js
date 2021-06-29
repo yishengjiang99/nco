dc25c1094d5530b2db15542874ab26878e86e648
Yisheng Jiang <yishengjiang@yahoo.com>
2021-06-28 18:20:00 -0700

    aafff

--- playsample.js
+++ playsample.js
@@ -1,71 +1,75 @@
-import {draw} from '../chart.js';
+import { draw } from "./chart.js";
 // "<script type='module' src='./playsample.js'></script>";
 import { fftmod } from "./fft-64bit/FFT.js";
-let fftmodw;let fftInputBuffer=new Float32Array(256);
+let fftmodw;
+let fftInputBuffer = new Float32Array(256);
 let fftspectraRef;
-fftmod(12).then(mod=>{
-	fftspectraRef=new Float64Array(mod.heap,mod.complexRef,2048)
-	fftmodw=mod;
+fftmod(12).then((mod) => {
+  fftspectraRef = new Float64Array(mod.heap, mod.complexRef, 2048);
+  fftmodw = mod;
 });
 let pt, ctx, sharedBuffer;
 const pcmCache = new WeakMap();
-const activeChains=[];
+const activeChains = [];
 
-const canvas=document.createElement("canvas");
+const canvas = document.createElement("canvas");
 canvas.setAttribute("width", 1029);
 canvas.setAttribute("height", 300);
 // const canvas2=document.createElement("canvas");
 // canvas2.setAttribute("width", 300);
 // canvas2.setAttribute("height", 250);
 
-const canvasContainer=document.createElement('div');
-canvasContainer.style='position:fixed;right:0;top:0px;display:grid;align-items:right';
+const canvasContainer = document.createElement("div");
+canvasContainer.style =
+  "position:fixed;right:0;top:0px;display:grid;align-items:right";
 canvasContainer.appendChild(canvas);
 // canvasContainer.appendChild(canvas2);
 
 document.body.append(canvasContainer);
-const canvasDraw=draw(()=>{
-	
-	return new Float64Array(fftmodw.heap,fftmodw.complexRef,4096);
-	
-},()=>[],4096,canvas);
+const canvasDraw = draw(
+  () => {
+    return new Float64Array(fftmodw.heap, fftmodw.complexRef, 4096);
+  },
+  () => [],
+  4096,
+  canvas
+);
 
-function renderSynth({abs, pt, lpf,modEmvelope,volumeEnveope}){
-	const panel =document.querySelector("aside");
-	document.querySelector("header").innerHTML= `<div>
+function renderSynth({ abs, pt, lpf, modEmvelope, volumeEnveope }) {
+  const panel = document.querySelector("aside");
+  document.querySelector("header").innerHTML = `<div>
 	<span>src:${abs.playbackRate.value}</span>
 	<meter value='${volumeEnveope.gain.value}' max='2'></meter>
 	<span>filter cutff${lpf.frequency.value}' <input type='range' value=${lpf.frequency}></input></span>`; //+	panel.innerHTML
 }
 
-async function init(){
-	document.querySelector("header").innerHTML='loading';
-	ctx = new AudioContext()
-	// sharedBuffer = new SharedArrayBuffer((10 + 4096 * 4) * Float64Array.BYTES_PER_ELEMENT);
-	// await ctx.audioWorklet.addModule(pt_code());
-};
-function effectsChain(idx,ctx){
-	if(!activeChains[idx]){
-
-	}
+async function init() {
+  document.querySelector("header").innerHTML = "loading";
+  ctx = new AudioContext();
+  // sharedBuffer = new SharedArrayBuffer((10 + 4096 * 4) * Float64Array.BYTES_PER_ELEMENT);
+  // await ctx.audioWorklet.addModule(pt_code());
+}
+function effectsChain(idx, ctx) {
+  if (!activeChains[idx]) {
+  }
 }
 async function processClicks(e) {
-	if (e.target.classList.contains("fftbins"))
-	{
-		e.preventDefault(); playbins();
-	}
-	if (e.target.classList.contains("attlist"))
-	{
-		e.preventDefault();
-		if (ctx.state == 'suspended') ctx.resume().then(() => processClicks(e));
+  if (e.target.classList.contains("fftbins")) {
+    e.preventDefault();
+    playbins();
+  }
+  if (e.target.classList.contains("attlist")) {
+    e.preventDefault();
+    if (ctx.state == "suspended") ctx.resume().then(() => processClicks(e));
 
-		const attrs = e.target.getAttribute("attrs").split(",");
+    const attrs = e.target.getAttribute("attrs").split(",");
 
-		document.querySelector("#details").innerHTML = attrs.map((attr, index) => `${genstrs[index]}:${attr}`).join('<br>')
-		return;
-	}
-	if (e.target.classList.contains('pcm'))
-	{
+    document.querySelector("#details").innerHTML = attrs
+      .map((attr, index) => `${genstrs[index]}:${attr}`)
+      .join("<br>");
+    return;
+  }
+  if (e.target.classList.contains("pcm")) {
     e.preventDefault();
     const attbag = e.target.parentElement.querySelector(".attlist");
     if (attbag == null) return;
@@ -151,32 +155,32 @@ async function processClicks(e) {
     // const analy = new AnalyserNode(ctx, { fftSize: 4096 });
     const ghetoproc = ctx.createScriptProcessor(256, 1, 1);
 
-		ghetoproc.onaudioprocess=({inputBuffer,outputBuffer})=>{
-			inputBuffer.copyFromChannel(outputBuffer.getChannelData(0),0);
-			inputBuffer.copyFromChannel(fftInputBuffer,0);
-			queueMicrotask(()=>fftmodw.inputPCM(fftInputBuffer));
-			 return true;
-		}
-		abs
-    
+    ghetoproc.onaudioprocess = ({ inputBuffer, outputBuffer }) => {
+      inputBuffer.copyFromChannel(outputBuffer.getChannelData(0), 0);
+      inputBuffer.copyFromChannel(fftInputBuffer, 0);
+      queueMicrotask(() => fftmodw.inputPCM(fftInputBuffer));
+      return true;
+    };
+    abs
+
       .connect(volumeEnveope)
-    
+
       .connect(lpf)
-    
+
       .connect(ghetoproc)
-    
+
       .connect(ctx.destination);
 
     abs.start();
     canvasDraw.start();
     // canvasDraw2.start();
     const timer = setInterval(() => {
-			fftspectraRef=fftmodw.getFloatFrequencyData();
+      fftspectraRef = fftmodw.getFloatFrequencyData();
     }, 1);
     abs.onended = () => {
       clearInterval(timer);
       canvasDraw.stop();
-    //  canvasDraw2.stop();
+      //  canvasDraw2.stop();
     };
 
     e.target.addEventListener(
@@ -198,11 +202,75 @@ async function processClicks(e) {
   }
 }
 init();
-window.onmousedown=processClicks;
+window.onmousedown = processClicks;
 
-const genstr = () => ["StartAddrOfs", "EndAddrOfs", "StartLoopAddrOfs", "EndLoopAddrOfs", "StartAddrCoarseOfs", "ModLFO2Pitch", "VibLFO2Pitch", "ModEnv2Pitch", "FilterFc", "FilterQ", "ModLFO2FilterFc", "ModEnv2FilterFc", "EndAddrCoarseOfs", "ModLFO2Vol", "Unused1", "ChorusSend", "ReverbSend", "Pan", "Unused2", "Unused3", "Unused4", "ModLFODelay", "ModLFOFreq", "VibLFODelay", "VibLFOFreq", "ModEnvDelay", "ModEnvAttack", "ModEnvHold", "ModEnvDecay", "ModEnvSustain", "ModEnvRelease", "Key2ModEnvHold", "Key2ModEnvDecay", "VolEnvDelay", "VolEnvAttack", "VolEnvHold", "VolEnvDecay", "VolEnvSustain", "VolEnvRelease", "Key2VolEnvHold", "Key2VolEnvDecay", "Instrument", "Reserved1", "KeyRange", "VelRange", "StartLoopAddrCoarseOfs", "Keynum", "Velocity", "Attenuation", "Reserved2", "EndLoopAddrCoarseOfs", "CoarseTune", "FineTune", "SampleId", "SampleModes", "Reserved3", "ScaleTune", "ExclusiveClass", "OverrideRootKey", "Dummy"];
+const genstr = () => [
+  "StartAddrOfs",
+  "EndAddrOfs",
+  "StartLoopAddrOfs",
+  "EndLoopAddrOfs",
+  "StartAddrCoarseOfs",
+  "ModLFO2Pitch",
+  "VibLFO2Pitch",
+  "ModEnv2Pitch",
+  "FilterFc",
+  "FilterQ",
+  "ModLFO2FilterFc",
+  "ModEnv2FilterFc",
+  "EndAddrCoarseOfs",
+  "ModLFO2Vol",
+  "Unused1",
+  "ChorusSend",
+  "ReverbSend",
+  "Pan",
+  "Unused2",
+  "Unused3",
+  "Unused4",
+  "ModLFODelay",
+  "ModLFOFreq",
+  "VibLFODelay",
+  "VibLFOFreq",
+  "ModEnvDelay",
+  "ModEnvAttack",
+  "ModEnvHold",
+  "ModEnvDecay",
+  "ModEnvSustain",
+  "ModEnvRelease",
+  "Key2ModEnvHold",
+  "Key2ModEnvDecay",
+  "VolEnvDelay",
+  "VolEnvAttack",
+  "VolEnvHold",
+  "VolEnvDecay",
+  "VolEnvSustain",
+  "VolEnvRelease",
+  "Key2VolEnvHold",
+  "Key2VolEnvDecay",
+  "Instrument",
+  "Reserved1",
+  "KeyRange",
+  "VelRange",
+  "StartLoopAddrCoarseOfs",
+  "Keynum",
+  "Velocity",
+  "Attenuation",
+  "Reserved2",
+  "EndLoopAddrCoarseOfs",
+  "CoarseTune",
+  "FineTune",
+  "SampleId",
+  "SampleModes",
+  "Reserved3",
+  "ScaleTune",
+  "ExclusiveClass",
+  "OverrideRootKey",
+  "Dummy",
+];
 function pt_code() {
-	return URL.createObjectURL(new Blob([`class PtProc extends AudioWorkletProcessor {
+  return URL.createObjectURL(
+    new Blob(
+      [
+        `class PtProc extends AudioWorkletProcessor {
 		constructor(options) {
 			super(options);
 			// this.woffset = woffset;
@@ -224,34 +292,36 @@ function pt_code() {
 		}
 	}
 	// @ts-ignore
-	registerProcessor("pt", PtProc);`], { 'type': 'application/javascript' }));
+	registerProcessor("pt", PtProc);`,
+      ],
+      { type: "application/javascript" }
+    )
+  );
 }
 
 function playbins(e) {
-
-	// rctx = rctx || new AudioContext();
-	// const real = e.target.getAttribute("real").split(",");
-	// const imag = e.target.getAttribute("img").split(",");
-	// let ctx = new OfflineAudioContext({
-	// 	numberOfChannels: 1,
-	// 	length: 4096,
-	// 	sampleRate: 4096,
-	// });
-
-	// let osc = new OscillatorNode(ctx, {
-	// 	type: "custom",
-	// 	periodicWave: new PeriodicWave(ctx, {
-	// 		imag, real
-	// 	}),
-	// 	frequency: 1,
-	// });
-	// osc.connect(ctx.destination);
-	// osc.start();
-	// osc.stop(1.0);
-	// const ab = (await ctx.startRendering());
-	// const abs = new AudioBufferSourceNode(rctx, {buffer: ab, loop: true});
-	// abs.start(); abs.stop(1);
-	// const curve = ab.getChannelData(0);
-	// //	chart(curve, document.querySelector("canvas"));
-	// return;
+  // rctx = rctx || new AudioContext();
+  // const real = e.target.getAttribute("real").split(",");
+  // const imag = e.target.getAttribute("img").split(",");
+  // let ctx = new OfflineAudioContext({
+  // 	numberOfChannels: 1,
+  // 	length: 4096,
+  // 	sampleRate: 4096,
+  // });
+  // let osc = new OscillatorNode(ctx, {
+  // 	type: "custom",
+  // 	periodicWave: new PeriodicWave(ctx, {
+  // 		imag, real
+  // 	}),
+  // 	frequency: 1,
+  // });
+  // osc.connect(ctx.destination);
+  // osc.start();
+  // osc.stop(1.0);
+  // const ab = (await ctx.startRendering());
+  // const abs = new AudioBufferSourceNode(rctx, {buffer: ab, loop: true});
+  // abs.start(); abs.stop(1);
+  // const curve = ab.getChannelData(0);
+  // //	chart(curve, document.querySelector("canvas"));
+  // return;
 }
